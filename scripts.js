89c9867f622132a38647b4f4c6f017f3694b6452
Rivaldo1123 <ramanlewis@gmail.com>
2020-11-01 16:50:46 -0500

    Sprinkle of Javascript

--- scripts.js
+++ scripts.js
@@ -1,1 +1,40 @@
 /* Add your JavaScript to this file */
+function main() {
+    window.onload = function () {
+        var mail;
+        var mess = document.getElementsByClassName("message")[0];
+        var sub = document.getElementsByTagName("button")[0];
+        sub.setAttribute("type", "button");
+        sub.onclick = function () { mailSub() }
+
+        function mailSub() {
+            mail = document.getElementById("email").value;
+            if (mail === "") {
+                change();
+            }
+            else {
+                thanks();
+            }
+
+            function change() {
+                var email = document.getElementsByName("email")[0];
+                email.value = '';
+                email.placeholder = "Please enter a valid email address";
+            }
+
+            function thanks() {
+                if (!email.value.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)) {
+                    change();
+                }
+                else {
+                    mess.innerHTML = 'Thank You! Your email address ' + mail + ' has been added to our mailing list';
+                    mess.style.textAlign = 'center';
+                    var email2 = document.getElementsByName("email")[0];
+                    email2.value = '';
+                    email2.placeholder = "Enter your email";
+                }
+            }
+        }
+    }
+}
+main();
